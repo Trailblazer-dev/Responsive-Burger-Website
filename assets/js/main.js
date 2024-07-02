04d3440c372c009365d13d9e07466233c4127949
Trailblazer-dev <richvictor830@gmail.com>
2024-07-02 18:16:39 +0300

    building

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,38 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+/*show menu */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*hide menu */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+const linkAction = ()=>{
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader =()=>{
+    const header = document.getElementById('header');
+    //Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >=50 ? header.classList.add('shadow-header'): header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll',shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
